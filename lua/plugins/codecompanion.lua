6351c0a534da057b6ada227fa40e72234a565439
Otto von Sperling <otto.sperling@transferwise.com>
2025-10-09 09:07:59 +0300

    WIP Make codecompanion inline work

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,13 +1,14 @@
 return {
     "olimorris/codecompanion.nvim",
+    lazy = false,
     -- dev = true,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "nvim-lua/plenary.nvim",
-        {
-            "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
-            opts = {},
-        },
+        -- {
+        --     "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+        --     opts = {},
+        -- },
     },
     cmd = {
         "CodeCompanion",
@@ -15,34 +16,69 @@ return {
         "CodeCompanionToggle",
         "CodeCompanionActions",
     },
-    config = function()
-        -- require("codecompanion").setup({
-        --     adapters = {
-        --         chat = require("codecompanion.adapters").use(
-        --             "ollama",
-        --             { schema = { model = { default = "qwen2.5-coder:3b" } } }
-        --         ),
-        --         inline = require("codecompanion.adapters").use(
-        --             "ollama",
-        --             { schema = { model = { default = "qwen2.5-coder:3b" } } }
-        --         ),
-        --     },
-        -- })
+    config = true,
+    init = function()
         require("codecompanion").setup({
+            adapters = {
+                ollama = function()
+                    return require("codecompanion.adapters").extend("ollama", {
+                        schema = {
+                            model = {
+                                default = "qwen3:8b",
+                                -- default = "deepseek-coder-v2:16b",
+                                -- default = 'codeqwen:7b',
+                            },
+                        },
+                        env = {
+                            url = "http://127.0.0.1:11434",
+                        },
+                    })
+                end,
+            },
             strategies = {
                 chat = {
-                    adapter = {
-                        name = "ollama",
-                        model = "qwen2.5-coder:3b",
-                    },
+                    adapter = "ollama",
                 },
                 inline = {
-                    adapter = {
-                        name = "ollama",
-                        model = "qwen2.5-coder:3b",
-                    },
+                    adapter = "ollama",
+                },
+                agent = {
+                    adapter = "ollama",
                 },
             },
         })
     end,
+    -- config = function()
+    --     require("codecompanion").setup({
+    --         adapters = {
+    --             chat = require("codecompanion.adapters").use(
+    --                 "ollama",
+    --                 { schema = { model = { default = "qwen3:8b" } } }
+    --             ),
+    --             inline = require("codecompanion.adapters").use(
+    --                 "ollama",
+    --                 { schema = { model = { default = "qwen3:8b" } } }
+    --             ),
+    --         },
+    --     })
+    -- end,
+    -- opts = {
+    --     strategies = {
+    --         chat = {
+    --             adapter = "ollama",
+    --             model = "qwen3:8b",
+    --         },
+    --         inline = {
+    --             adapter = "ollama",
+    --             model = "qwen3:8b",
+    --         },
+    --         cmd = {
+    --             adapter = "ollama",
+    --             model = "qwen3:8b",
+    --         },
+    --     },
+    --     opts = {
+    --         log_level = "DEBUG",
+    --     },
+    -- },
 }

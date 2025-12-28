5e091b8396ce4ccf9592d3879df33022de777aaf
Otto von Sperling <otto.sperling@transferwise.com>
2025-12-28 16:05:09 +0200

    WIP Fix CodeCompanion setup after breaking changes in 18.x

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,6 +1,6 @@
 return {
     "olimorris/codecompanion.nvim",
-    version = "v17.33.0",
+    version = "18.3.0",
     -- event = "LspAttach",
     -- enabled = true,
     dependencies = {
@@ -11,43 +11,101 @@ return {
         --     opts = {},
         -- },
     },
-    opts = {
-        cmd = {
-            "CodeCompanion",
-            "CodeCompanionChat",
-            "CodeCompanionToggle",
-            "CodeCompanionActions",
-        },
-        strategies = {
-            chat = {
-                adapter = "ollama",
-                model = "qwen2.5-coder:1.5b-base-q6_K",
-            },
-            inline = {
-                adapter = "ollama",
-                model = "qwen2.5-coder:1.5b-base-q6_K",
-                keymaps = {
-                    accept_change = {
-                        modes = { n = "ga" },
-                        description = "Accept the suggested change",
-                    },
-                    reject_change = {
-                        modes = { n = "gr" },
-                        opts = { nowait = true },
-                        description = "Reject the suggested change",
-                    },
+    config = function()
+        require("codecompanion").setup({
+            adapters = {
+                http = {
+                    ollama = function()
+                        return require("codecompanion.adapters").extend("openai_compatible", {
+                            name = "ollama",
+                            env = {
+                                url = "http://localhost:11434",
+                                allow_insecure = true,
+                                -- chat_url = "/v1/chat/completions",
+                            },
+                            schema = {
+                                model = {
+                                    default = "deepseek-r1:7b-qwen-distill-q4_K_M",
+                                },
+                            },
+                        })
+                    end,
                 },
             },
-            agent = {
-                adapter = "ollama",
-                model = "qwen2.5-coder:1.5b-base-q6_K",
+            strategies = {
+                chat = {
+                    adapter = "ollama",
+                },
+                inline = {
+                    adapter = "ollama",
+                },
+                agent = {
+                    adapter = "ollama",
+                },
             },
-        },
-        opts = {
-            log_level = "DEBUG",
-        },
-    },
+        })
+    end,
 }
+--     opts = {
+--         -- cmd = {
+--         --     "CodeCompanion",
+--         --     "CodeCompanionChat",
+--         --     "CodeCompanionToggle",
+--         --     "CodeCompanionActions",
+--         -- },
+--         -- adapters = {
+--         --     http = {
+--         --         ollama = function()
+--         --             return require("codecompanion.adapters").extend("ollama", {
+--         --                 schema = {
+--         --                     model = {
+--         --                         default = "deepseek-r1:7b-qwen-distill-q4_K_M",
+--         --                     },
+--         --                 },
+--         --                 env = {
+--         --                     url = "http://localhost:11434",
+--         --                     allow_insecure = true,
+--         --                     -- chat_url = "/v1/chat/completions",
+--         --                 },
+--         --             })
+--         --         end,
+--         --     },
+--         -- },
+--         interactions = {
+--             chat = {
+--                 adapter = { name = "ollama", model = "deepseek-r1:7b-qwen-distill-q4_K_M" },
+--                 -- adapter = "ollama",
+--             },
+--             -- inline = {
+--             --     adapter = {
+--             --         name = "ollama",
+--             --         model = "deepseek-r1:7b-qwen-distill-q4_K_M",
+--             --         keymaps = {
+--             --             accept_change = {
+--             --                 modes = { n = "ga" },
+--             --                 description = "Accept the suggested change",
+--             --             },
+--             --             reject_change = {
+--             --                 modes = { n = "gr" },
+--             --                 opts = { nowait = true },
+--             --                 description = "Reject the suggested change",
+--             --             },
+--             --         },
+--             --     },
+--             -- },
+--             background = {
+--                 adapter = { name = "ollama", model = "deepseek-r1:7b-qwen-distill-q4_K_M" },
+--             },
+--             -- agent = {
+--             --     adapter = "ollama",
+--             --     model = "qwen2.5-coder:7b-base-q6_K",
+--             -- },
+--         },
+--         opts = {
+--             log_level = "DEBUG",
+--         },
+--     },
+-- }
 -- config = true,
 -- init = function()
 --     require("codecompanion").setup({
